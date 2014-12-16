eabffee23cbea728387ee016a400588be26e19e4
Laura Whalin <laurawhalin@gmail.com>
2014-12-16 07:47:34 -0700

    initial commit

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'bundler'
+Bundler.require
+
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+    configure :development do
+      register Sinatra::Reloader
+    end
+
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+
+  not_found do
+    erb :error
+  end
+end
---
+++ idea.rb
@@ -0,0 +1,27 @@
+require 'yaml/store'
+
+class Idea
+  attr_reader :title, :description
+
+  def self.all
+    database.transaction do |db|
+      db['ideas'] || []
+    end
+  end
+
+  def initialize(title, description)
+    @title       = title
+    @description = description
+  end
+
+  def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: title, description: description}
+    end
+  end
+
+  def database
+    @database ||= YAML::Store.new "ideabox"
+  end
+end
